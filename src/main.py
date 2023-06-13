006aeff81acd618f2c0288dc97155f32ab3500fa
DanielApt <apt.daniel@gmail.com>
2023-06-13 23:27:10 +0100

    Create basic volume control

--- src/main.py
+++ src/main.py
@@ -1,14 +1,29 @@
 import pygame
 import os
+import keyboard
+# import RPi.GPIO as GPIO
 
+# GPIO.setmode(GPIO.BCM)
 directory = os.path.dirname(__file__)
 # audio_file = os.path.join(directory, './rain-2.mp3'
 audio_file = os.path.join(directory, '..', 'audio', 'rain-2.mp3')
+volume = 1
 
 pygame.mixer.init()
 pygame.mixer.music.load(audio_file)
+pygame.mixer.music.set_volume(volume)
 pygame.mixer.music.play(loops = -1)
 while pygame.mixer.music.get_busy() == True:
+    while True:
+        key = keyboard.read_key()
+
+        if key == 'down':
+            print(key)
+            volume -= 0.5
+
+        elif key == 'up':
+            volume += 0.5
+
+        pygame.mixer.music.set_volume(volume)
+
     continue
-
-
