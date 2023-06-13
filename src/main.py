1b86bb070d3632eda3056323a747a3fb8daeeb67
DanielApt <apt.daniel@gmail.com>
2023-06-13 23:32:11 +0100

    Cycle through volumes

--- src/main.py
+++ src/main.py
@@ -3,6 +3,9 @@ import os
 import keyboard
 # import RPi.GPIO as GPIO
 
+index = 0
+volumes = [0.25, 0.5, 0.75, 1, 0]
+
 # GPIO.setmode(GPIO.BCM)
 directory = os.path.dirname(__file__)
 # audio_file = os.path.join(directory, './rain-2.mp3'
@@ -11,19 +14,20 @@ volume = 1
 
 pygame.mixer.init()
 pygame.mixer.music.load(audio_file)
-pygame.mixer.music.set_volume(volume)
+pygame.mixer.music.set_volume(volumes[index])
 pygame.mixer.music.play(loops = -1)
+
 while pygame.mixer.music.get_busy() == True:
     while True:
         key = keyboard.read_key()
 
         if key == 'down':
             print(key)
-            volume -= 0.5
+            index -= 1
 
         elif key == 'up':
-            volume += 0.5
+            index += 1
 
-        pygame.mixer.music.set_volume(volume)
+        pygame.mixer.music.set_volume(volumes[index % len(volumes)])
 
     continue
